54ed1d585d68ad2fcf7971f91f8594e7e4342c1f
mainewong <m4inewong@gmail.com>
2022-07-12 12:07:08 +0800

    commit

--- constants/API.js
+++ constants/API.js
@@ -1,6 +1,7 @@
-export const API = "https://pcmob6-blog-api.mainewong.repl.co"
+export const API = "https://pcmob7-project.mainewong.repl.co"
 export const API_LOGIN = "/auth"
 export const API_SIGNUP = "/newuser"
 export const API_POSTS = "/posts"
 export const API_WHOAMI = "/whoami"
 export const API_CREATE = "/create"
+cc
